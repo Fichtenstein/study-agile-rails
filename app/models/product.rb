6a924ff80a10507e73872285f0cba26dc3949765
Ignatius Fichtenstein <ignatiuss.92@gmail.com>
2017-10-04 17:22:16 +0000

    Iter D2. Line item impl.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,19 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true,
-    format: { with: %r{\.(gif|jpg|png)\Z}i,
-              message: "must be a URL for GIF, JPEG or PNG image." }
+            format: { with: %r{\.(gif|jpg|png)\Z}i,
+                      message: "must be a URL for GIF, JPEG or PNG image." }
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_li
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_li
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      thow :abort
+    end
+  end
 end
